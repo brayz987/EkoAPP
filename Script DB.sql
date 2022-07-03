641c959e7f866ca3f0df018aa928c5b1ca8924f0
brayz987 <brayz987@gmail.com>
2022-07-03 13:14:06 -0500

    Update Script DB.sql

--- Script DB.sql
+++ Script DB.sql
@@ -1,18 +1,15 @@
-
-
-
 CREATE TABLE `Residuo` (
   `id_residuo` int PRIMARY KEY AUTO_INCREMENT,
-  `peso` Float,
-  `cantidad` Float,
+  `peso` double,
+  `cantidad` double,
   `clasificacion` varchar(255),
-  `id_servicio` int
+  `id_servicio` int,
+  `id_tipoResiduo` int
 );
 
 CREATE TABLE `TipoResiduo` (
   `id_tipoResiduo` int PRIMARY KEY AUTO_INCREMENT,
-  `tipo` varchar(255),
-  `id_residuo` int
+  `tipo` varchar(255)
 );
 
 CREATE TABLE `Servicio` (
@@ -20,7 +17,7 @@ CREATE TABLE `Servicio` (
   `fecha_servicio` date,
   `fecha_cierre` date,
   `ciudad` varchar(255),
-  `pesoTotal` Float,
+  `pesoTotal` double,
   `direccion` varchar(255)
 );
 
@@ -29,13 +26,13 @@ CREATE TABLE `Usuario` (
   `nombre` varchar(255),
   `direccion` varchar(255),
   `correo` varchar(255),
-  `contacto` varchar(255)
+  `contacto` varchar(255),
+  `id_tipoUsuario` int
 );
 
 CREATE TABLE `TipoUsuario` (
-  `id_tipoResiduo` int PRIMARY KEY,
-  `description` varchar(255),
-  `identificacion_usuario` int
+  `id_tipoUsuario` int PRIMARY KEY AUTO_INCREMENT,
+  `description` varchar(255)
 );
 
 CREATE TABLE `ServicioXUsuario` (
@@ -53,9 +50,9 @@ CREATE TABLE `Sedes` (
 
 ALTER TABLE `Residuo` ADD FOREIGN KEY (`id_servicio`) REFERENCES `Servicio` (`id_servicio`);
 
-ALTER TABLE `TipoResiduo` ADD FOREIGN KEY (`id_residuo`) REFERENCES `Residuo` (`id_residuo`);
+ALTER TABLE `Residuo` ADD FOREIGN KEY (`id_tipoResiduo`) REFERENCES `TipoResiduo` (`id_tipoResiduo`);
 
-ALTER TABLE `TipoUsuario` ADD FOREIGN KEY (`identificacion_usuario`) REFERENCES `Usuario` (`identificacion_usuario`);
+ALTER TABLE `Usuario` ADD FOREIGN KEY (`id_tipoUsuario`) REFERENCES `TipoUsuario` (`id_tipoUsuario`);
 
 ALTER TABLE `ServicioXUsuario` ADD FOREIGN KEY (`identificacion_usuario`) REFERENCES `Usuario` (`identificacion_usuario`);
 
