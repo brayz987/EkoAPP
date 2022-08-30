b7585d16e04b833dd8e9ae370a041016e3ab2446
Richardeiby <107088694+Richardeiby@users.noreply.github.com>
2022-08-29 23:26:45 -0500

    nuevo sql

---
+++ ekoapp.sql
@@ -0,0 +1,375 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-08-2022 a las 06:20:16
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ekoapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `listado_inventario`
+--
+
+CREATE TABLE `listado_inventario` (
+  `Inventario_id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `listado_inventario`
+--
+
+INSERT INTO `listado_inventario` (`Inventario_id`, `nombre`) VALUES
+(1, 'Botella'),
+(2, 'Envases'),
+(3, 'Metales'),
+(4, 'Tetrabrick'),
+(5, 'Corcho'),
+(6, 'Plástico'),
+(7, 'Vidrio'),
+(8, 'Pilas '),
+(9, 'Baterías'),
+(10, 'Madera');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localidad`
+--
+
+CREATE TABLE `localidad` (
+  `localidad_id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `localidad`
+--
+
+INSERT INTO `localidad` (`localidad_id`, `nombre`) VALUES
+(1, 'USAQUEN'),
+(2, 'CHAPINERO'),
+(3, 'SANTA FE'),
+(4, 'SAN CRISTOBAL'),
+(5, 'USME'),
+(6, 'TUNJUELITO'),
+(7, 'BOSA'),
+(8, 'KENNEDY'),
+(9, 'FONTIBON'),
+(10, 'ENGATIVA'),
+(11, 'SUBA'),
+(12, 'BARRIOS UNIDOS'),
+(13, 'TEUSAQUILLO'),
+(14, 'MARTIRES'),
+(15, 'ANTONIO NARIÑO'),
+(16, 'PUENTE ARANDA'),
+(17, 'CANDELARIA'),
+(18, 'RAFAEL URIBE'),
+(19, 'CIUDAD BOLIVAR'),
+(20, 'SUMAPAZ');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `residuo`
+--
+
+CREATE TABLE `residuo` (
+  `id_residuo` int(11) NOT NULL,
+  `peso` double DEFAULT NULL,
+  `cantidad` double DEFAULT NULL,
+  `id_servicio` int(11) DEFAULT NULL,
+  `id_listado_inventario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `residuo`
+--
+
+INSERT INTO `residuo` (`id_residuo`, `peso`, `cantidad`, `id_servicio`, `id_listado_inventario`) VALUES
+(24, 5, 15, 24, 2),
+(25, 4, 4, 24, 4),
+(26, 3, 4, 24, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sedes`
+--
+
+CREATE TABLE `sedes` (
+  `id_sede` int(11) NOT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `servicio` varchar(255) DEFAULT NULL,
+  `identificacion_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `id_servicio` int(11) NOT NULL,
+  `fecha_servicio` date DEFAULT NULL,
+  `fecha_cierre` date DEFAULT NULL,
+  `pesoTotal` double DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `id_tiporesiduo` int(11) NOT NULL,
+  `estado` varchar(30) NOT NULL,
+  `localidad_id` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `servicio`
+--
+
+INSERT INTO `servicio` (`id_servicio`, `fecha_servicio`, `fecha_cierre`, `pesoTotal`, `direccion`, `id_tiporesiduo`, `estado`, `localidad_id`, `idUser`) VALUES
+(19, '2022-08-21', NULL, 2131, 'prueba edicion 2', 4, 'eliminado', 15, 2),
+(20, '2022-07-07', NULL, 30, 'Autopista Sur', 5, 'eliminado', 5, 2),
+(21, '2022-07-26', NULL, 16, 'Mi casa', 6, 'eliminado', 2, 2),
+(22, '2022-07-29', NULL, 50, 'La casa de otro', 2, 'eliminado', 8, 2),
+(23, '2022-07-05', NULL, 40, 'Nuevo servicio', 4, 'eliminado', 11, 2),
+(24, '2022-07-06', '2022-08-21', 50, 'Mi casa', 1, 'Cerrado', 14, 2),
+(25, '2022-07-06', NULL, 50, 'Mi casa', 1, 'Pendiente', 14, 2),
+(26, '0000-00-00', NULL, 50, 'Puente Peatonal', 5, 'Pendiente', 20, 2),
+(27, '2022-08-11', NULL, 32, 'dsadas', 1, 'Pendiente', 3, 2),
+(28, '2022-08-05', NULL, 12, 'prueba servicios cliente', 1, 'Pendiente', 5, 2),
+(29, '2022-08-11', NULL, 212, 'prueba user id', 3, 'Pendiente', 1, 2),
+(30, '2022-08-11', NULL, 212, 'prueba user id', 3, 'Pendiente', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiposresiduoservicio`
+--
+
+CREATE TABLE `tiposresiduoservicio` (
+  `id_tiporesiduo` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tiposresiduoservicio`
+--
+
+INSERT INTO `tiposresiduoservicio` (`id_tiporesiduo`, `nombre`) VALUES
+(1, 'Residuos domésticos'),
+(2, 'Residuos comerciales'),
+(3, 'Residuos Industriales'),
+(4, 'Biorresiduos'),
+(5, 'Escombros y residuos de construccion'),
+(6, 'Residuos sanitarios');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipousuario`
+--
+
+CREATE TABLE `tipousuario` (
+  `id_tipoUsuario` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tipousuario`
+--
+
+INSERT INTO `tipousuario` (`id_tipoUsuario`, `description`) VALUES
+(1, 'Cliente'),
+(2, 'Colaborador'),
+(3, 'Administrador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `identificacion_usuario` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `correo` varchar(255) DEFAULT NULL,
+  `contacto` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `id_tipoUsuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `identificacion_usuario`, `nombre`, `direccion`, `correo`, `contacto`, `password`, `id_tipoUsuario`) VALUES
+(1, 213245356, 'Richard', 'pasdasgber', 'richard@admin.com', '3002223445', 'k,mjynthbgrvfd', 2),
+(2, 1003706375, 'Brayan Cardenas', 'Cr 4 # 59 - 84', 'brayz987@gmail.com', '3502649117', 'NuevoPass', 2),
+(3, 1233436465, 'Juanito Alcachofas', 'Cr 4 # 59 - 90', 'jachofas@admin.com', '3502649117', 'mntbtdv ', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `listado_inventario`
+--
+ALTER TABLE `listado_inventario`
+  ADD PRIMARY KEY (`Inventario_id`);
+
+--
+-- Indices de la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  ADD PRIMARY KEY (`localidad_id`);
+
+--
+-- Indices de la tabla `residuo`
+--
+ALTER TABLE `residuo`
+  ADD PRIMARY KEY (`id_residuo`),
+  ADD KEY `id_servicio` (`id_servicio`),
+  ADD KEY `listado_inventario` (`id_listado_inventario`);
+
+--
+-- Indices de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  ADD PRIMARY KEY (`id_sede`),
+  ADD KEY `identificacion_usuario` (`identificacion_usuario`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`id_servicio`),
+  ADD KEY `tipoResiduoServicio` (`id_tiporesiduo`),
+  ADD KEY `localidad` (`localidad_id`),
+  ADD KEY `userxservicio` (`idUser`);
+
+--
+-- Indices de la tabla `tiposresiduoservicio`
+--
+ALTER TABLE `tiposresiduoservicio`
+  ADD PRIMARY KEY (`id_tiporesiduo`);
+
+--
+-- Indices de la tabla `tipousuario`
+--
+ALTER TABLE `tipousuario`
+  ADD PRIMARY KEY (`id_tipoUsuario`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_tipoUsuario` (`id_tipoUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `listado_inventario`
+--
+ALTER TABLE `listado_inventario`
+  MODIFY `Inventario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  MODIFY `localidad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT de la tabla `residuo`
+--
+ALTER TABLE `residuo`
+  MODIFY `id_residuo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  MODIFY `id_sede` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT de la tabla `tiposresiduoservicio`
+--
+ALTER TABLE `tiposresiduoservicio`
+  MODIFY `id_tiporesiduo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `tipousuario`
+--
+ALTER TABLE `tipousuario`
+  MODIFY `id_tipoUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `residuo`
+--
+ALTER TABLE `residuo`
+  ADD CONSTRAINT `listado_inventario` FOREIGN KEY (`id_listado_inventario`) REFERENCES `listado_inventario` (`Inventario_id`),
+  ADD CONSTRAINT `residuo_ibfk_1` FOREIGN KEY (`id_servicio`) REFERENCES `servicio` (`id_servicio`);
+
+--
+-- Filtros para la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  ADD CONSTRAINT `sedesUsers` FOREIGN KEY (`identificacion_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Filtros para la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD CONSTRAINT `localidad` FOREIGN KEY (`localidad_id`) REFERENCES `localidad` (`localidad_id`),
+  ADD CONSTRAINT `tipoResiduoServicio` FOREIGN KEY (`id_tiporesiduo`) REFERENCES `tiposresiduoservicio` (`id_tiporesiduo`),
+  ADD CONSTRAINT `userxservicio` FOREIGN KEY (`idUser`) REFERENCES `usuario` (`id`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_tipoUsuario`) REFERENCES `tipousuario` (`id_tipoUsuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
